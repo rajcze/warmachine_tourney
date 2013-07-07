0bfc05e0169979dd7f6ed30938df024912dd5201
Josef Skladanka <josef.skladanka@gmail.com>
2013-07-07 12:12:35 +0200

    Mostly working. File load/save dialogs to be added.
    Signed-off-by: Josef Skladanka <josef.skladanka@gmail.com>

--- gui.py
+++ gui.py
@@ -28,35 +28,6 @@ class PMainWindow(QtGui.QMainWindow):
         self.ui.e_pBcp.setFixedSize(metrics.width("8888"), self.ui.e_pBcp.height())
         self.ui.e_pAkp.setFixedSize(metrics.width("888888"), self.ui.e_pAkp.height())
         self.ui.e_pBkp.setFixedSize(metrics.width("888888"), self.ui.e_pBkp.height())
-    
-    def _t_players_add_player(self, player):
-        rown = self.ui.t_players.rowCount()
-        self.ui.t_players.insertRow(rown)
-        self.ui.t_players.setItem(rown, 0, QtGui.QTableWidgetItem("%s" % player.uid))
-        self.ui.t_players.setItem(rown, 1, QtGui.QTableWidgetItem("%s" % player.name))
-        self.ui.t_players.setItem(rown, 2, QtGui.QTableWidgetItem("%s" % player.faction))
-        self.ui.t_players.setItem(rown, 3, QtGui.QTableWidgetItem("%s" % player.team))
-        self.ui.t_players.setItem(rown, 4, QtGui.QTableWidgetItem("%s" % player.country))
-        self.ui.t_players.setItem(rown, 5, QtGui.QTableWidgetItem("%s" % player.tp))
-        self.ui.t_players.setItem(rown, 6, QtGui.QTableWidgetItem("%s" % player.cp))
-        self.ui.t_players.setItem(rown, 7, QtGui.QTableWidgetItem("%s" % player.kp))
-        for column in range(self.ui.t_players.columnCount() - 1):
-            self.ui.t_players.resizeColumnToContents(column)
-
-    
-    @QtCore.Slot()
-    def on_b_addPlayer_clicked(self):
-        name = self.ui.e_name.text()
-        faction = self.ui.c_faction.currentText()
-        team = self.ui.e_team.text()
-        country = self.ui.e_country.text()
-        
-        uid = self.ui.t_players.rowCount()+1
-        p = Player(name, faction, team, country, uid = uid)
-        self.tournament.add_player(p)
-        self._t_players_add_player(p)
-        
-        self.tournament.tables = len(self.tournament.players) / 2
 
     def __guiclear(self):
         self.ui.t_players.clearContents()
@@ -64,6 +35,7 @@ class PMainWindow(QtGui.QMainWindow):
         self.ui.t_pairings.clearContents()
         self.ui.t_pairings.setRowCount(0)
         self.ui.c_pairRound.clear()
+        self.ui.b_addPlayer.setEnabled(True)
 
     def yes_no_dialog(self, title, question):
         flags = QtGui.QMessageBox.StandardButton.Yes 
@@ -76,22 +48,111 @@ class PMainWindow(QtGui.QMainWindow):
         
         return False
 
-    @QtCore.Slot(bool)
-    def on_actionLoad_Players_triggered(self, state):
-        if  not self.yes_no_dialog("Load players?", "This will reset the tournament state. Continue?"):
-            return
+    # ============ TAB PLAYERS ============
     
-        import csv_worker
+    def __t_players_fill_row(self, rown, player):
+        self.ui.t_players.setItem(rown, 0, QtGui.QTableWidgetItem("%s" % player.uid))
+        self.ui.t_players.setItem(rown, 1, QtGui.QTableWidgetItem("%s" % player.name))
+        self.ui.t_players.setItem(rown, 2, QtGui.QTableWidgetItem("%s" % player.faction))
+        self.ui.t_players.setItem(rown, 3, QtGui.QTableWidgetItem("%s" % player.team))
+        self.ui.t_players.setItem(rown, 4, QtGui.QTableWidgetItem("%s" % player.country))
+        self.ui.t_players.setItem(rown, 5, QtGui.QTableWidgetItem("%s" % player.tp))
+        self.ui.t_players.setItem(rown, 6, QtGui.QTableWidgetItem("%s" % player.sos))
+        self.ui.t_players.setItem(rown, 7, QtGui.QTableWidgetItem("%s" % player.cp))
+        self.ui.t_players.setItem(rown, 8, QtGui.QTableWidgetItem("%s" % player.kp))
+        for column in range(self.ui.t_players.columnCount() - 1):
+            self.ui.t_players.resizeColumnToContents(column)
+    
+    def __t_players_sort_tournament_order(self):
+        for column in [8,7,6,5]:
+            self.ui.t_players.sortItems(column, QtCore.Qt.SortOrder.DescendingOrder)
+    
+    def t_players_add_player(self, player):
+        rown = self.ui.t_players.rowCount()
+        self.ui.t_players.insertRow(rown)
+        self.__t_players_fill_row(rown, player)
 
-        self.tournament.clear()
-        self.__guiclear()
-        for p in csv_worker.read_players():
-            uid = self.ui.t_players.rowCount()+1
-            p = Player(uid = uid, **p)
-            self.tournament.add_player(p)
-            self._t_players_add_player(p)
+    def update_t_players_from_tournament(self):
+        self.ui.t_players.clearContents()
+        self.ui.t_players.setRowCount(0)
+        for uid, p in self.tournament.players.iteritems():
+            self.t_players_add_player(p)        
+        self.__t_players_sort_tournament_order()
+
+    # ------------ Add Player ------------
+    
+    @QtCore.Slot()
+    def on_b_addPlayer_clicked(self):
+        name = self.ui.e_name.text()
+        faction = self.ui.c_faction.currentText()
+        team = self.ui.e_team.text()
+        country = self.ui.e_country.text()
+        
+        uid = self.ui.t_players.rowCount()+1
+        p = Player(name, faction, team, country, uid = uid)
+        self.tournament.add_player(p)
+        self.t_players_add_player(p)
         
         self.tournament.tables = len(self.tournament.players) / 2
+    
+    # ------------ Edit Player ------------    
+    
+    def __editplayer_guiclear(self):
+        self.ui.e_name_2.setText("")
+        self.ui.e_team_2.setText("")
+        self.ui.e_country_2.setText("")
+
+    @QtCore.Slot(str)
+    def on_e_pUid_textEdited(self, text):
+        try:
+            p = self.tournament.players[text]
+        except KeyError:
+            self.__editplayer_guiclear()
+            return
+            
+        self.ui.e_name_2.setText(p.name)
+        index = self.ui.c_faction_2.findText("%s" % p.factions[0])
+        self.ui.c_faction_2.setCurrentIndex(index)
+        self.ui.e_team_2.setText(p.team)
+        self.ui.e_country_2.setText(p.country)
+
+    @QtCore.Slot()
+    def on_b_editPlayer_clicked(self):
+        try:
+            p = self.tournament.players[self.ui.e_pUid.text()]
+        except KeyError:
+            QtGui.QMessageBox.warning(self, "Failure", "Player with ID %s does not exist." % self.ui.e_pUid.text())
+            return
+        
+        name = self.ui.e_name_2.text()
+        faction = self.ui.c_faction_2.currentText()
+        team = self.ui.e_team_2.text()
+        country = self.ui.e_country_2.text()
+
+        if not name:
+            QtGui.QMessageBox.warning(self, "Failure", "Name must be filled.")
+            return
+        
+        if not self.yes_no_dialog("Save changes", "Are you sure?"):
+            return
+        
+        p.name = name
+        p.factions[0] = faction
+        p.team = team
+        p.country = country
+        
+        rown = 0
+        for rown in range(self.ui.t_players.rowCount()):
+            if  self.ui.t_players.item(rown, 0).text() == p.uid:
+                break
+        self.__t_players_fill_row(rown, p)
+        
+        self.ui.e_pUid.setText("")
+        self.__editplayer_guiclear()
+        self._show_pairings(self.tournament.pairings[-1], self.tournament.byes[-1])
+        
+
+    # ============ TAB PAIRINGS ============
 
     def _show_pairings(self, pairings, bye):
         tp = self.ui.t_pairings 
@@ -143,6 +204,9 @@ class PMainWindow(QtGui.QMainWindow):
         self.ui.c_pairRound.setCurrentIndex(index)
 
         self.ui.statusbar.showMessage("Current round: %s" % r)
+        self.ui.b_addPlayer.setEnabled(False)
+    
+    # ============ TAB RESULTS ============
     
     def __addResultGuiClear(self):
         self.ui.e_pAuid.setText("")
@@ -184,7 +248,6 @@ class PMainWindow(QtGui.QMainWindow):
         for faction in pair[1].factions:
             self.ui.c_pBfaction.addItem(faction)
 
-        
         # if the players already played, prefill also the tp/cp/kp/...
         if len(pair[0]._tp) == (self.tournament.current_round + 1):
             self.ui.e_pAtp.setText(str(pair[0]._tp[-1]))
@@ -216,8 +279,7 @@ class PMainWindow(QtGui.QMainWindow):
         cround = self.tournament.current_round
         
         if (pA.tables_played[cround] != table) or (pB.tables_played[cround] != table):
-            #FIXME: add big fat warning
-            print "These players did not play on table %s in round %s" % (table, cround + 1)
+            QtGui.QMessageBox.critical(self, "Critical error", "These players did not play on table %s in round %s" % (table, cround + 1))
             return
         
         pAtp = self.ui.e_pAtp.text()
@@ -228,14 +290,13 @@ class PMainWindow(QtGui.QMainWindow):
         pBkp = self.ui.e_pBkp.text()
         
         if not (pAtp and pAcp and pAkp and pBtp and pBcp and pBkp):
-            #FIXME: add big fat warning
-            print "You need to fill all the results"
+            QtGui.QMessageBox.information(self, "Information", "You need to fill all the results")
             return
         
         #editing the results
         if len(pA._tp) == cround + 1:        
-            #FIXME: add big fat warning
-            print "EDITING RESULTS"
+            if not self.yes_no_dialog("Edit Results", "You are changing once filled results. Are you sure?"):
+                return
             pA._tp[-1] = int(pAtp)
             pA._cp[-1] = int(pAcp)
             pA._kp[-1] = int(pAkp)
@@ -255,7 +316,29 @@ class PMainWindow(QtGui.QMainWindow):
             pB._kp.append(int(pBkp))
             pB.factions_played.append(self.ui.c_pAfaction.currentText())
         
+        self.update_t_players_from_tournament()
         self.__addResultGuiClear()
+        self.ui.e_tblnum.setText("")
+
+    # ============ FILE MENU ============
+
+    @QtCore.Slot(bool)
+    def on_actionLoad_Players_triggered(self, state):
+        if  not self.yes_no_dialog("Load players?", "This will reset the tournament state. Continue?"):
+            return
+    
+        import csv_worker
+
+        self.tournament.clear()
+        self.__guiclear()
+        for p in csv_worker.read_players():
+            uid = self.ui.t_players.rowCount()+1
+            p = Player(uid = uid, **p)
+            self.tournament.add_player(p)
+            self.t_players_add_player(p)
+        
+        self.tournament.tables = len(self.tournament.players) / 2
+
 
     @QtCore.Slot(bool)
     def on_actionSave_tournament_state_triggered(self, state):
@@ -270,7 +353,10 @@ class PMainWindow(QtGui.QMainWindow):
         # fill players table
         self.__guiclear()
         for p_uid in sorted(self.tournament.players.keys()):
-            self._t_players_add_player(self.tournament.players[p_uid])
+            self.t_players_add_player(self.tournament.players[p_uid])
+        
+        # sort them according to Masters 2013
+        self.__t_players_sort_tournament_order()
         
         # fill current pairing table
         for i in range(self.tournament.current_round +1):
@@ -282,6 +368,9 @@ class PMainWindow(QtGui.QMainWindow):
         
         # update status bar
         self.ui.statusbar.showMessage("Current round: %s" % (self.tournament.current_round + 1))
+        
+        if self.tournament.current_round > -1:
+            self.ui.b_addPlayer.setEnabled(False)
         
 
 if __name__ == "__main__":
